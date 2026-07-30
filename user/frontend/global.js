83e547adabd1674c93cfcd3dbd5cb31750dc687d
Rishab Raj <rr0948555@gmail.com>
2026-07-30 21:56:33 +0530

    Adding images from the database

--- user/frontend/global.js
+++ user/frontend/global.js
@@ -42,6 +42,16 @@ function requestBanners() {
                  slide.style.backgroundImage = `url("http://localhost/ShoppingCart/${banner.image}")`;
                  slide.style.height = "45vh";
                  slide.style.backgroundSize = "cover";
+                 const h3 = document.createElement('h3');
+                 h3.textContent = banner.name;
+                 const p = document.createElement('p');
+                 p.textContent = banner.description;
+                 const button = document.createElement('button');
+                 button.textContent = 'Shop Now';
+                 slide.appendChild(h3);
+                 slide.appendChild(p);
+                 slide.appendChild(button);
+
                  const swipeWrapper = document.querySelector(".swiper-wrapper");
                   swipeWrapper.append(slide);
               });
