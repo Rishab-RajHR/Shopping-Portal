0b0c9bbfb8b1839f74aadb68ed7485e900c37c5c
Rishab Raj <rr0948555@gmail.com>
2026-08-04 14:05:04 +0530

    Login

--- user/frontend/login.js
+++ user/frontend/login.js
@@ -3,6 +3,12 @@ const register = document.querySelector(".register");
 const logout = document.querySelector(".logout");
 const loggedUser = document.querySelector(".logged-user");
 
+login.addEventListener("click", userLogin);
+
+function userLogin(e) {
+    e.preventDefault();
+}
+
 showHideIcon(register, true);
 function showHideIcon(icon, flag) {
     flag ? (icon.style.display = "none") : (icon.style.display = "block");
